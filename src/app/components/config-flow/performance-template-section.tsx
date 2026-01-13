5dc091cea58220cdd9191fda648c16945a32411f
Nisala Perera <nisalaperera@gmail.com>
2026-01-13 21:14:28 +0000

    I can't see these changes

--- src/app/components/config-flow/performance-template-section.tsx
+++ src/app/components/config-flow/performance-template-section.tsx
@@ -4,7 +4,7 @@
 import { useState, useMemo, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, DialogFooter } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
