2b920ce38e0933be031a8759a07a95c190703ec5
Nisala Perera <nisalaperera@gmail.com>
2026-01-13 08:07:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1299472069-5e3f0",
+  "appId": "1:319672925420:web:05f756461ceb407cf439c9",
+  "apiKey": "AIzaSyD_i4bDi9ZY7DrW14XNPMnlso6b_1sTFJE",
+  "authDomain": "studio-1299472069-5e3f0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "319672925420"
+};
